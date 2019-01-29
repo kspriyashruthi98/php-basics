8e3ecb66e5a4b999d3a4c2f81ed2f007cd1d51e0
shanmugapriya <kspriya.shruthi@gmail.com>
2019-01-29 18:33:21 +0530

    Initial Commit

---
+++ if-else.php
@@ -0,0 +1,13 @@
+<?php
+$a=10;
+$b=4;
+if($a<$b)
+{
+    echo "$a is smaller value";
+}
+else
+{
+    echo "$b is greater value";
+}
+
+?>
---
+++ if.php
@@ -0,0 +1,11 @@
+<?php
+$a=10;
+$b=5;
+if($a>$b)
+{
+echo "Bigger value is $a";
+}
+else{
+echo "smaller value is $b";
+}
+?>
